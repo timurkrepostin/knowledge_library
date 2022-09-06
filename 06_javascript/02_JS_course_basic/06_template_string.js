defe42988555391974e424c6351fa5534926364d
timurkrepostin <100711086+timurkrepostin@users.noreply.github.com>
2022-09-06 23:30:50 +0300

    template_string

--- 06_javascript/02_JS_course_basic/06_template_string.js
+++ 06_javascript/02_JS_course_basic/06_template_string.js
@@ -2,4 +2,27 @@
 // Шаблонные строки
 
 const firstName = 'Denis';
-const lastName = 'Petrov';
+const lastName = 'Mescheryakov';
+const age = 30;
+let str;
+
+str = 'Hello my name is ' + firstName + ' ' + lastName; // таким образом можно сформировать строку, особенно для маленькой строки
+// но для удобства можно сделать иначе, на примере шаблона html
+str = '<ul>' +
+      '<li>First Name: ' + firstName + '</li>' +
+      '<li>Last Name: ' + lastName + '</li>' +
+      '<li>Age: ' + age + '</li>' +
+      '</ul>';
+// console.log(str);
+
+// ES6; // `` это новый синтаксис объявления строк, с возможностью добавления в них переменных(доллар+фигурные скобки)
+str = `
+<ul>
+  <li>First name ${ firstName }</li>
+  <li>Last name ${ lastName }</li>
+  <li>Age ${ age }</li>
+  <li>Math.random ${ Math.random() }</li>
+  <li>5 + 5: ${ 5 + 5 }<li/>
+</ul>
+`; 
+document.body.innerHTML = str;
